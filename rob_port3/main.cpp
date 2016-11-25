85caf649034218f63d7d03f7ac59b15916a4067f
Katrine Nielsen <katrinemnielsen@me.com>
2016-11-25 12:50:35 +0100

    Initial Commit

---
+++ rob_port3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rob_port3
+//
+//  Created by Katrine Maria Nielsen on 25/11/16.
+//  Copyright © 2016 Katrine Maria Nielsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
